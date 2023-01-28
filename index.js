705d3566eef821b8f9e067aecd7d176e8147eef9
sachin <ysachin511@gmail.com>
2023-01-28 22:06:42 +0530

    url

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
     cors:{
-        origin:"http://localhost:3000",
+        origin:"https://visionary-semifreddo-6d74c4.netlify.app",
         methods:["GET","POST"]
     }
 })
